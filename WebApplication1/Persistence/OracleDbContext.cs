5e2291bdda598a0fb1f3d5730738a06dbdd3a496
David Augusto <davidribeiro086@gmail.com>
2023-08-22 19:50:16 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Persistence/OracleDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Persistence
+{
+    public class OracleDbContext : DbContext
+    {
+        public DbSet<Alunos> Alunos{ get; set; }
+
+        public OracleDbContext(DbContextOptions<OracleDbContext> options) : base (options) 
+        {
+
+        }
+    }
+}
